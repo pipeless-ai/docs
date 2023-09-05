f6f2f5fc35d56f062e474febe2992316e9a7bc27
Miguel A. Cabrera Minagorri <devgorri@gmail.com>
2023-09-05 17:43:43 +0200

    Revert "Add middleware to load resources"
    This reverts commit dab94feaa2e978f905c21510858cf7c61b2420cb.

--- middleware.js
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from 'next/server';
-
-export default async function middleware(req) {
-  const { pathname } = req.nextUrl
-  if (pathname.startsWith('/docs')) {
-    return NextResponse.rewrite(new URL(pathname, "https://docs.pipeless.ai"))
-  }
-  return NextResponse.next()
-}

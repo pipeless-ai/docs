bf8d67dd52801e52cb1ca3850adfc7879b5ae352
Miguel A. Cabrera Minagorri <devgorri@gmail.com>
2023-09-05 16:41:05 +0200

    Add logo link
    Signed-off-by: Miguel A. Cabrera Minagorri <devgorri@gmail.com>

--- theme.config.tsx
+++ theme.config.tsx
@@ -4,7 +4,8 @@ import { DocsThemeConfig } from 'nextra-theme-docs'
 import { BsTwitter } from 'react-icons/bs';
 
 const config: DocsThemeConfig = {
-  logo: <span style={{display: "flex", alignItems: "center"}}><Image alt="logo" width={40} height={40} src="/logo-400x400-rounded.png" />&nbsp;<b>Pipeless</b></span>,
+  logo: <span style={{display: "flex", alignItems: "center"}}><Image alt="logo" width={30} height={30} src="/logo-400x400-rounded.png" />&nbsp;<b>Pipeless</b></span>,
+  logoLink: 'https://pipeless.ai',
   project: {
     link: 'https://github.com/miguelaeh/pipeless',
   },

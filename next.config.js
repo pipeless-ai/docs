504695dfc8e6c1ae91caae356e96530d5769934a
Miguel A. Cabrera Minagorri <devgorri@gmail.com>
2023-09-10 18:06:43 +0200

    use NODE_ENV instead of VERCEL
    Signed-off-by: Miguel A. Cabrera Minagorri <devgorri@gmail.com>

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const withNextra = require('nextra')({
 })
 
 module.exports = withNextra({
-	assetPrefix: process.env.NEXT_PUBLIC_VERCEL_ENV === 'production' ? '/docs' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/docs' : '',
   webpack(config) {
     const allowedSvgRegex = /components\/icons\/.+\.svg$/
 

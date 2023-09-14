4b4e1aee504495a52225321ec2ab4b94043b5185
Miguel A. Cabrera Minagorri <devgorri@gmail.com>
2023-09-14 11:33:50 +0200

    Update docs sitemap
    Signed-off-by: Miguel A. Cabrera Minagorri <devgorri@gmail.com>

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 // Trick the URL to point to the main site
-const siteUrl = 'https://pipeless.ai/docs';
+const siteUrl = 'https://www.pipeless.ai/docs';
 module.exports = {
     siteUrl,
     generateRobotsTxt: true,

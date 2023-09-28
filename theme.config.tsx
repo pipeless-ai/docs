66d568d69d9fcd653ce2c2eff9ddaebbbfa5b63b
Miguel A. Cabrera Minagorri <devgorri@gmail.com>
2023-09-28 17:34:06 +0200

    Add Discord link
    Signed-off-by: Miguel A. Cabrera Minagorri <devgorri@gmail.com>

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Image from 'next/image'
 import { DocsThemeConfig } from 'nextra-theme-docs'
-import { BsTwitter } from 'react-icons/bs';
+import { BsDiscord } from 'react-icons/bs';
 
 const config: DocsThemeConfig = {
   logo: <span style={{display: "flex", alignItems: "center"}}><Image alt="logo" width={30} height={30} src="/docs/logo-400x400-rounded.png" />&nbsp;<b>Pipeless</b></span>,
@@ -11,8 +11,8 @@ const config: DocsThemeConfig = {
     link: 'https://github.com/miguelaeh/pipeless',
   },
   chat: {
-    link: 'https://twitter.com/pipeless_ai',
-    icon: <BsTwitter size={25} />
+    link: 'https://discord.gg/K2qxQ8uedG',
+    icon: <BsDiscord size={25} />
   },
   docsRepositoryBase: 'https://github.com/pipeless/docs',
   footer: {
